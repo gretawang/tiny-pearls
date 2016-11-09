0548ae95d09011d2dd5cb4dd0ce98e7f30b9fa28
GretaWang <greta.wang@leandev.se>
2016-11-09 17:17:03 +0800

    Add a test class.
    Add a test class.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,18 @@
 package main.java;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
 /**
  * @author: grewan
  * @date: 2016/11/9
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is a test class.");
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     }
 }
