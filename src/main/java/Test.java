d8bc5429306f44d831100ce9f59ac917af762491
GretaWang <greta.wang@leandev.se>
2016-11-09 17:05:40 +0800

    Add a test class.
    Add a test class.

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * @author: grewan
+ * @date: 2016/11/9
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is a test class.");
+    }
+}
