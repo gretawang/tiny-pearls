eefc7d8e7110b3e3aaf4d88182ac8af7ff48e036
GretaWang <greta.wang@leandev.se>
2017-10-13 10:17:54 +0800

    Add mongodb demo codes.
    Add mongodb demo codes.

---
+++ src/main/space/gretawang/ty/mongodbdemo/MongodbDemo.java
@@ -0,0 +1,22 @@
+package main.space.gretawang.ty.mongodbdemo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Mongodb version: 3.4.0
+ * Mongodb driver version: 3.5.0
+ */
+public class MongodbDemo {
+    public static void main(String args[]) {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://10.10.4.139:27020"));
+        MongoDatabase database = mongoClient.getDatabase("test");
+
+        for (String name : database.listCollectionNames()) {
+            System.out.println(name);
+        }
+
+
+    }
+}
