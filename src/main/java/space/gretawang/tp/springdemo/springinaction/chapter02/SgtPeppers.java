b92ef6e47a5e99f0871e75a8d50ba5f50a909986
grewan <greta.wang@leandev.se>
2017-11-22 10:55:04 +0800

    Add spring demo.
    Add spring demo.

--- src/main/java/space/gretawang/tp/springdemo/springinaction/chapter02/SgtPeppers.java
+++ src/main/java/space/gretawang/tp/springdemo/springinaction/chapter02/SgtPeppers.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 /**
  * @Component注解表明,这个类会作为组件类,spring会为该类创建Bean
  * 那么这里的问题就来了, 这个Bean会在什么时候创建,是系统启动的时候,还是使用的时候?
+ *
+ * 所有带@Component注解的类都会被创建成bean, @ComponentScan注解负责扫描
  */
 
 @Component
